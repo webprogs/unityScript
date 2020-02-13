24b7b42b4970617c90a5b93b9af3a703792a72f0
holychild <holychild@pc.com>
2020-02-13 09:27:12 +0800

    First Person Camera

---
+++ FPcamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPcamera : MonoBehaviour
+{
+ 	// Start is called before the first frame update
+    public float speedX = 2.0f;
+    public float speedY = 2.0f;
+
+
+    private float Horizontal = 0.0f;
+    private float Vertical = 0.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal += speedX * Input.GetAxis("Mouse X");
+        Vertical -= speedY * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles =  new Vector3(Vertical, Horizontal, 0.0f);
+    }
+}
