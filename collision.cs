94e6d9c3708dff58afa7d6bf543a4f583020b246
holychild <holychild@pc.com>
2020-02-13 09:16:44 +0800

    add collision script

---
+++ collision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    void OnCollisionEnter (Collision col) {
+    	if (col.gameObject.tag == "box") {
+    		Destroy(col.gameObject);
+    	}
+    }
+}
