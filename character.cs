25005cc715529d186c2f94d69cfd055dd07f7e8d
holychild <holychild@pc.com>
2020-02-13 08:38:44 +0800

    init

---
+++ character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    // basic animation
+    Animator anim;
+    void Start()
+    {
+	    anim =  GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    	if (Input.GetKey(KeyCode.W)) {
+    		Debug.Log(0);
+	        anim.SetInteger ("action", 1);
+    	} else if (Input.GetKey(KeyCode.S)) {
+	        anim.SetInteger ("action", 2);
+    	} else {
+	        anim.SetInteger ("action", 0);
+    	}
+    }
+}
