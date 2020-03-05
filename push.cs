26e73c8ae0034e11368acd6fc2b7bce06ebc5cd3
holychild <holychild@pc.com>
2020-03-05 09:51:17 +0800

    push ball

---
+++ push.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("s")) {
+        	rb.AddForce(Vector3.forward * 500);
+        }
+    }
+}
