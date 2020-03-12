1c8a635a4ca247d67b4d26bfffa601a88100a2cf
holychild <holychild@pc.com>
2020-03-12 09:42:01 +0800

    score

---
+++ score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text txt;
+    public GameObject myScore;
+    
+    void Start()
+    {
+        txt = myScore.GetComponent<Text>();
+        txt.text = "Change Text";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
